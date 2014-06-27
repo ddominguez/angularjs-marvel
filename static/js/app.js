4641055ef4137d2d5c32a4fc8e1ad5da5fde8888
David Dominguez <ddominguez@gmail.com>
2014-06-27 18:36:01 -0400

    added app js

---
+++ static/js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var marvelApp = angular.module('marvelApp', []);
+marvelApp.controller("comicsController", function($scope, $http) {
+    $scope.comics = {};
+    $http({
+        method: 'GET',
+        url: 'http://gateway.marvel.com:80/v1/public/comics?characters=1009313&apikey='+MARVEL_API_KEY
+    }).
+    success(function(data, status, headers, config) {
+        $scope.comics = data.data.results;
+        console.log($scope.comics);
+    });
+});
