71009a1fed47e020a0fac7b732d16442d8e7fa5f
David Dominguez <ddominguez@gmail.com>
2014-07-07 22:48:16 -0400

    added comic controller * added service to get comics uri

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 'use strict';
+
 var characterId = 1009313;
 // var characterId = 1009368;
 var app = angular.module('marvelApp', []);
 
-app.controller("characterController", ['$scope', '$http', function($scope, $http) {
+app.factory('comicsService', function($rootScope) {
+    var comicsService = {};
+    comicsService.comicsURI = '';
+
+    comicsService.prepForBroadcast = function(uri) {
+        this.comicsURI = uri;
+        this.broadcastItem();
+    };
+
+    comicsService.broadcastItem = function() {
+        $rootScope.$broadcast('handleBroadcast');
+    };
+
+    return comicsService;
+});
+
+app.controller("characterController", ['$scope', '$http', 'comicsService', function($scope, $http, comicsService) {
     $scope.data = {};
     $http({
         method: 'GET',
@@ -11,6 +28,26 @@ app.controller("characterController", ['$scope', '$http', function($scope, $http
     }).
     success(function(data, status, headers, config) {
         $scope.data = data.data.results[0];
-        console.log($scope.data);
+        comicsService.prepForBroadcast($scope.data.comics.collectionURI);
     });
+}]);
+
+app.controller('comicsController', ['$scope', '$http', 'comicsService', function($scope, $http, comicsService) {
+    $scope.data = {};
+    $scope.foo = 'foo';
+    $scope.$on('handleBroadcast', function() {
+        $scope.comicsURI = comicsService.comicsURI;
+        $scope.requestComics();
+    });
+
+    $scope.requestComics = function() {
+        $http({
+            method: 'GET',
+            url: $scope.comicsURI+'?apikey='+MARVEL_API_KEY
+        }).
+        success(function(data, status, headers, config) {
+            $scope.data = data.data.results;
+            console.log($scope.data);
+        });
+    }
 }]);
