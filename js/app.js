4c45d6999e63449858968f688d65bf73c5e0dda8
David Dominguez <ddominguez@gmail.com>
2014-07-30 22:00:19 -0400

    Getting marvel events

--- js/app.js
+++ js/app.js
@@ -2,27 +2,30 @@
 
 var app = angular.module('marvelApp', ['ngAnimate', 'ngResource']);
 
-app.controller("characterController", ['$scope', '$resource', function($scope, $resource) {
-    var characters = $resource('http://gateway.marvel.com:80/v1/public/:entity/:id/:collection',
-        {entity:'characters', apikey:MARVEL_API_KEY});
+app.controller("eventsController", ['$scope', '$resource', function($scope, $resource) {
+    var events = $resource('http://gateway.marvel.com:80/v1/public/:entity/:id/:collection',
+        {entity:'events', apikey:MARVEL_API_KEY, orderBy:'name'});
+    $scope.comicCount = 1;
 
     // get characters list
-    $scope.charactersResult = characters.get(function() {
+    $scope.eventsResult = events.get(function() {
         // get first character id from result
-        var defaultCharacterId = $scope.charactersResult.data.results[0].id;
-        $scope.getCharacterInfo(defaultCharacterId);
+        var defaultId = $scope.eventsResult.data.results[0].id;
+        $scope.getInfo(defaultId);
     });
 
-    $scope.getCharacterInfo = function(id) {
-        var characterInfoResult = characters.get({id: id}, function() {
-            $scope.characterInfo = characterInfoResult.data.results[0];
-            $scope.getCharacterComics(id);
+    $scope.getInfo = function(id) {
+        $scope.loadingContent = true;
+        var infoResult = events.get({id: id}, function() {
+            $scope.info = infoResult.data.results[0];
+            $scope.getComics(id);
         });
     };
 
-    $scope.getCharacterComics = function(id) {
-        var comicsResult = characters.get({id:id, collection:'comics'}, function() {
-            $scope.characterComics = comicsResult.data.results;
+    $scope.getComics = function(id) {
+        var comicsResult = events.get({id:id, collection:'comics', orderBy:''}, function() {
+            $scope.loadingContent = false;
+            $scope.comics = comicsResult.data.results;
             $scope.comicCount = comicsResult.data.count;
         });
     }
